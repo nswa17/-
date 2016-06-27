96b58d081a67cb98ad83094fd9903c269a6f9d72
nswa17 <nswa17@gmail.com>
2016-06-27 17:50:07 +0900

    Add 分布を指定できる成績生成関数

--- functions.jl
+++ functions.jl
@@ -1,10 +1,14 @@
-function get_scores(student_num, mu, sigma2, sigma2_error)
+function get_scores(student_num, mu::Float64, sigma2::Float64, sigma2_error::Float64)
     #生徒の成績が平均mu, 分散sigma2の正規分布に従うと仮定.
     #成績には分散sigma2_errorの誤差が混入すると仮定
-    scores = mu + sqrt(sigma2 + sigma2_error) * randn(student_num)
-    return scores
+    return mu + sqrt(sigma2 + sigma2_error) * randn(student_num)
 end
-"
+
+function get_scores(student_num, distribution, error_distribution)
+    #生徒の成績が与えられた分布に従うと仮定.
+    #成績には与えられた分布の誤差が混入すると仮定
+    return [rand(distribution) + rand(error_distribution) for i in 1:student_num]
+end
+
 using PyPlot
 plot(get_scores(500, 50, 20, 4))
-"
