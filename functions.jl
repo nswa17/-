c6c230b3e8e828cf0762e7847cb9fcc591d44309
nswa17 <nswa17@gmail.com>
2016-06-27 23:55:05 +0900

    facultyの選好を偏らせるのではなく生徒の選好を偏らせるように変更

--- functions.jl
+++ functions.jl
@@ -57,8 +57,11 @@ function generate_students(students_num, mu, sigma2, sigma2_error, faculty_num; 
     return students_list
 end
 
-function get_sorted_students_list(students_list, faculty)#alpha...学部の文系度合い, 学部の理系度合い（それぞれ[0, 1])の値を取る
-    return sort(students_list, by=x->x.score+faculty.alpha*x.score_science+faculty.beta*x.score_liberal_arts)
+function get_sorted_faculties_id_list(faculties_list, student)#alpha...学部の文系度合い, 学部の理系度合い（それぞれ[0, 1])の値を取る
+    available_faculties_list = shuffle(faculties_list)[1:rand(1:length(faculties_list))]#Todo: 自分が応募できるfacultyのみに手が出せるようにする予定
+    sorted_faculties_id_list = zeros(Int, length(faculties_list)+1)
+    sorted_faculties_id_list[1:length(available_faculties_list)] = map(f -> f.id, sort(available_faculties_list, by = f -> -abs(f.alpha-student.score_science)-abs(f.beta-student.score_liberal_arts)))#sort関数は適当
+    return sorted_faculties_id_list
 end
 
 type Faculty
@@ -81,7 +84,7 @@ end
 
 function set_prefs_faculties(faculties_list, students_list)
     for faculty in faculties_list
-        sorted_students_list = get_sorted_students_list(students_list, faculty)
+        sorted_students_list = sort(students_list, by = s -> s.score)
         for i in 1:length(students_list)+1
             if i < faculty.cap
                 faculty.prefs[i] = sorted_students_list[i].id
@@ -96,8 +99,7 @@ end
 
 function set_prefs_students(students_list, faculties_list)#random
     for student in students_list
-        student.prefs[1:end-1] = map(f -> f.id, shuffle(faculties_list))
-        student.prefs[end] = 0
+        student.prefs = get_sorted_faculties_id_list(faculties_list, student)
     end
 end
 
@@ -113,7 +115,9 @@ function generate_caps(as)
     return Int[a.cap for a in as]
 end
 
-
+function evaluate_matched(s_matched, s_prefs)
+    return sum([findfirst(s_prefs[:, i], r) for (i, r) in enumerate(s_matched)])/size(s_prefs, 2)
+end
 #####以下デバッグ用
 faculty_num = 2
 students_num = 10
@@ -133,7 +137,7 @@ f_prefs = generate_prefs(faculties_list)
 caps = generate_caps(faculties_list)
 
 #println(s_prefs)
-println(f_prefs)
+#println(f_prefs)
 
 s_matched, f_matched, indptr = DA.call_match(s_prefs, f_prefs, caps)
 
