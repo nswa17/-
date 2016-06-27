9d72ef437907224a9ac557379951a714aa8501c3
nswa17 <nswa17@gmail.com>
2016-06-28 00:18:21 +0900

    勘違い修正. demo追加.

--- functions.jl
+++ functions.jl
@@ -1,4 +1,5 @@
 include("da.jl")
+#自分の好きなところに成績に関係なく応募すると仮定
 
 function get_score(mu, sigma2, sigma2_error)
     #生徒の成績が平均mu, 分散sigma2の正規分布に従うと仮定.
@@ -15,44 +16,28 @@ end
 type Student
     id::Int
     score::Float64
-    score_science::Float64
-    score_liberal_arts::Float64
+    characteristic::Float64
     faculty::Int#所属する科類 文一 => 1, 理一 => 4
     prefs::Array{Int, 1}
 end
 
-function generate_students(students_num, distribution, error_distribution, faculty_num; indep = false)
+function generate_students(students_num, distribution, error_distribution, faculty_num)
     students_list = Array(Student, students_num)
     id = 1
     for i in 1:students_num
-        if indep####Studentsの成績と観測不能な理系・文系への適正が全て独立な場合
-            score = get_score(distribution, error_distribution)
-            score_science = get_score(distribution, error_distribution)
-            score_liberal_arts = get_score(distribution, error_distribution)
-            students_list[i] = Student(i, score, score_science, score_liberal_arts, 1, Array(Int, faculty_num+1))#1はとりあえず
-        else
-            score = get_score(distribution, error_distribution)
-            score_science = students_list[i].score(1 + 0.2*(rand() + 0.5))
-            score_science = students_list[i].score(1 + 0.2*(rand() + 0.5))
-            students_list[i] = Student(i, score, score_science, score_liberal_arts, 1, Array(Int, faculty_num+1))
-        end
+        score = get_score(distribution, error_distribution)
+        score_sub = get_score(distribution, error_distribution)
+        students_list[i] = Student(i, score, score_sub, 1, Array(Int, faculty_num+1))#1はとりあえず
     end
+    return students_list
 end
 
-function generate_students(students_num, mu, sigma2, sigma2_error, faculty_num; indep=false)
+function generate_students(students_num, mu, sigma2, sigma2_error, faculty_num)
     students_list = Array(Student, students_num)
-    for i in 1:students_num####Studentsの成績と観測不能な理系・文系への適正が全て独立な場合
-        if indep
-            score = get_score(mu, sigma2, sigma2_error)
-            score_science = get_score(mu, sigma2, sigma2_error)
-            score_liberal_arts = get_score(mu, sigma2, sigma2_error)
-            students_list[i] = Student(i, score, score_science, score_liberal_arts, 1, Array(Int, faculty_num+1))
-        else
-            score = get_score(mu, sigma2, sigma2_error)
-            score_science = get_score(mu, sigma2, sigma2_error)
-            score_liberal_arts = get_score(mu, sigma2, sigma2_error)
-            students_list[i] = Student(i, score, score_science, score_liberal_arts, 1, Array(Int, faculty_num+1))
-        end
+    for i in 1:students_num
+        score = get_score(mu, sigma2, sigma2_error)
+        characteristic = get_score(mu, sigma2, sigma2_error)
+        students_list[i] = Student(i, score, characteristic, 1, Array(Int, faculty_num+1))
     end
     return students_list
 end
@@ -60,22 +45,21 @@ end
 function get_sorted_faculties_id_list(faculties_list, student)#alpha...学部の文系度合い, 学部の理系度合い（それぞれ[0, 1])の値を取る
     available_faculties_list = shuffle(faculties_list)[1:rand(1:length(faculties_list))]#Todo: 自分が応募できるfacultyのみに手が出せるようにする予定
     sorted_faculties_id_list = zeros(Int, length(faculties_list)+1)
-    sorted_faculties_id_list[1:length(available_faculties_list)] = map(f -> f.id, sort(available_faculties_list, by = f -> -abs(f.alpha-student.score_science)-abs(f.beta-student.score_liberal_arts)))#sort関数は適当
+    sorted_faculties_id_list[1:length(available_faculties_list)] = map(f -> f.id, sort(available_faculties_list, by = f -> -abs(f.characteristic-student.characteristic)))#sort関数は適当
     return sorted_faculties_id_list
 end
 
 type Faculty
     id::Int
     prefs::Array{Int, 1}
-    alpha::Float64
-    beta::Float64
+    characteristic::Float64
     cap::Int
 end
 
 function generate_faculties(faculty_num, students_num)
     faculties_list = Array(Faculty, faculty_num)
     for i in 1:faculty_num
-        faculties_list[i] = Faculty(i, Array(Int, students_num+1), rand(), rand(), 5)#5はキャップ数(とりあえず)
+        faculties_list[i] = Faculty(i, Array(Int, students_num+1), rand(), students_num-2)#students_num-2はキャップ数(とりあえず)
     end
     return faculties_list
 end
@@ -118,6 +102,7 @@ end
 function evaluate_matched(s_matched, s_prefs)
     return sum([findfirst(s_prefs[:, i], r) for (i, r) in enumerate(s_matched)])/size(s_prefs, 2)
 end
+
 #####以下デバッグ用
 faculty_num = 2
 students_num = 10
