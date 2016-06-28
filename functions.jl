07911e7f8846216fa2509b551f36ad9fdc057454
nswa17 <nswa17@gmail.com>
2016-06-28 21:24:45 +0900

    学科を選ぶときのpreferenceと自分の成績のtradeoffを実装.

--- functions.jl
+++ functions.jl
@@ -1,33 +1,42 @@
 include("da.jl")
-#自分の好きなところに成績に関係なく応募すると仮定
 
-function get_score(mu, sigma2, sigma2_error)
+type Student
+    id::Int
+    level::Float64#0.0~1.0に変換すると使いやすい?
+    preference::Float64
+    current_faculty::Int#所属する科類 文一 => 1, 理一 => 4
+    prefs::Array{Int, 1}
+    emphasis_on_preference::Float64#0.0~1.0 どれくらい自分の選考を成績よりも重視するか
+end
+
+type Faculty
+    id::Int
+    prefs::Array{Int, 1}
+    preference::Float64
+    level::Float64
+    cap::Int
+    available_for::Array{Int, 1}#その類の人のみがfacultyに応募できる.
+end
+
+function get_level(mu, sigma2, sigma2_error)
     #生徒の成績が平均mu, 分散sigma2の正規分布に従うと仮定.
     #成績には分散sigma2_errorの誤差が混入すると仮定
     return mu + sqrt(sigma2 + sigma2_error) * randn()
 end
 
-function get_score(distribution, error_distribution)
+function get_level(distribution, error_distribution)
     #生徒の成績が与えられた分布に従うと仮定.
     #成績には与えられた分布の誤差が混入すると仮定
     return rand(distribution) + rand(error_distribution)
-end
-
-type Student
-    id::Int
-    score::Float64
-    characteristic::Float64
-    faculty::Int#所属する科類 文一 => 1, 理一 => 4
-    prefs::Array{Int, 1}
 end
 
 function generate_students(students_num, distribution, error_distribution, faculty_num)
     students_list = Array(Student, students_num)
     id = 1
     for i in 1:students_num
-        score = get_score(distribution, error_distribution)
-        score_sub = get_score(distribution, error_distribution)
-        students_list[i] = Student(i, score, score_sub, 1, Array(Int, faculty_num+1))#1はとりあえず
+        level = get_level(distribution, error_distribution)
+        preference = get_level(distribution, error_distribution)
+        students_list[i] = Student(i, level, preference, rand(1:6), Array(Int, faculty_num+1), 0.5)#0.5は適当
     end
     return students_list
 end
@@ -35,32 +44,27 @@ end
 function generate_students(students_num, mu, sigma2, sigma2_error, faculty_num)
     students_list = Array(Student, students_num)
     for i in 1:students_num
-        score = get_score(mu, sigma2, sigma2_error)
-        characteristic = get_score(mu, sigma2, sigma2_error)
-        students_list[i] = Student(i, score, characteristic, 1, Array(Int, faculty_num+1))
+        level = get_level(mu, sigma2, sigma2_error)
+        preference = get_level(mu, sigma2, sigma2_error)
+        students_list[i] = Student(i, level, preference, rand(1:6), Array(Int, faculty_num+1), 0.5)
     end
     return students_list
 end
 
-function get_sorted_faculties_id_list(faculties_list, student)#alpha...学部の文系度合い, 学部の理系度合い（それぞれ[0, 1])の値を取る
-    available_faculties_list = shuffle(faculties_list)[1:rand(1:length(faculties_list))]#Todo: 自分が応募できるfacultyのみに手が出せるようにする予定
-    sorted_faculties_id_list = zeros(Int, length(faculties_list)+1)
-    sorted_faculties_id_list[1:length(available_faculties_list)] = map(f -> f.id, sort(available_faculties_list, by = f -> -abs(f.characteristic-student.characteristic)))#sort関数は適当
+function get_sorted_faculties_id_list(faculties_list, student)
+    available_faculties_list = filter(f -> in(student.current_faculty, f.available_for), faculties_list)
+    sorted_faculties_id_list = Array(Int, length(faculties_list)+1)
+    sorted_faculties_id_list[1:length(available_faculties_list)] = map(f -> f.id, sort(available_faculties_list, by = f -> -student.emphasis_on_preference*abs(f.preference-student.preference)-(1-student.emphasis_on_preference)*abs(f.level-student.level)))#sort関数は適当
+    sorted_faculties_id_list[length(available_faculties_list) + 1] = 0
+    #println(length())
+    sorted_faculties_id_list[(length(available_faculties_list) + 2):end] = map(f -> f.id, filter(f -> !in(f, available_faculties_list), faculties_list))
     return sorted_faculties_id_list
-end
-
-type Faculty
-    id::Int
-    prefs::Array{Int, 1}
-    characteristic::Float64
-    level::Float64
-    cap::Int
 end
 
 function generate_faculties(faculty_num, students_num)
     faculties_list = Array(Faculty, faculty_num)
     for i in 1:faculty_num
-        faculties_list[i] = Faculty(i, Array(Int, students_num+1), rand(), rand(), students_num-2)#students_num-2はキャップ数(とりあえず)
+        faculties_list[i] = Faculty(i, Array(Int, students_num+1), rand(), rand(), students_num-1, [rand(1:6)])#students_num-1はキャップ数(とりあえず)
     end
     return faculties_list
 end
@@ -69,7 +73,7 @@ end
 
 function set_prefs_faculties(faculties_list, students_list)
     for faculty in faculties_list
-        sorted_students_list = sort(students_list, by = s -> s.score)
+        sorted_students_list = sort(students_list, by = s -> s.level)
         for i in 1:length(students_list)+1
             if i < faculty.cap
                 faculty.prefs[i] = sorted_students_list[i].id
@@ -82,7 +86,7 @@ function set_prefs_faculties(faculties_list, students_list)
     end
 end
 
-function set_prefs_students(students_list, faculties_list)#random
+function set_prefs_students(students_list, faculties_list)
     for student in students_list
         student.prefs = get_sorted_faculties_id_list(faculties_list, student)
     end
@@ -100,16 +104,24 @@ function generate_caps(as)
     return Int[a.cap for a in as]
 end
 
-function evaluate_matched(s_matched, s_prefs)
+function evaluate_matched(s_matched, s_prefs)#min:1
     return sum([findfirst(s_prefs[:, i], r) for (i, r) in enumerate(s_matched)])/size(s_prefs, 2)
 end
 
+function evaluate_matched2(s_matched, s_prefs)#min:1
+    return sqrt(sum([findfirst(s_prefs[:, i], r)^2 for (i, r) in enumerate(s_matched)])/size(s_prefs, 2))
+end
+
+function evaluate_matched3(s_matched, s_prefs)#min:1
+    return 2 + sum([-1/findfirst(s_prefs[:, i], r) for (i, r) in enumerate(s_matched)])/size(s_prefs, 2)
+end
+
 #####以下デバッグ用
-faculty_num = 2
+faculty_num = 4
 students_num = 10
-mu = 50
-sigma2 = 30
-sigma2_error = 5
+mu = 0.5
+sigma2 = 0.2
+sigma2_error = 0.05
 
 faculties_list = generate_faculties(faculty_num, students_num)
 students_list = generate_students(students_num, mu, sigma2, sigma2_error, faculty_num)
@@ -128,5 +140,8 @@ caps = generate_caps(faculties_list)
 s_matched, f_matched, indptr = DA.call_match(s_prefs, f_prefs, caps)
 
 println(s_matched, f_matched)
+println(evaluate_matched(s_matched, s_prefs))
+println(evaluate_matched2(s_matched, s_prefs))
+println(evaluate_matched3(s_matched, s_prefs))
 #sorted_students_list = get_sorted_students_list(students_list, faculties_list)
 #println(sorted_students_list)
